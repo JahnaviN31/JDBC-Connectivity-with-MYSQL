ab72c566d1a6b61ff4f7cb2f2ba8ef3092e75860
JahnaviN31 <jahnavi.naru@gmail.com>
2023-11-03 22:38:12 +0530

    Added Java file

---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+class Test {
+    public static void main(String args[]) {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampledatabase", "root", "password");
+            System.out.println("Connection Success");
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+}
